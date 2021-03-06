3f5c3296b8eff6bbeab67cd92a77ae768f826449
Carleton-SRCL <80171095+Carleton-SRCL@users.noreply.github.com>
2021-03-06 13:31:33 -0500

    Add files via upload

---
+++ Projects/Alex_Experiments_DEPRECIATED/Combined_Experiment/include/TCP_Server.h
@@ -0,0 +1,12 @@
+#include "rtwtypes.h"
+
+void createServerNewSocket();
+void run_guidance(const double input_1, const double input_2, 
+                const double input_3, const double input_4,
+                const double input_5, const double input_6,
+                const double input_7, const double input_8, 
+                const double input_9, const double input_10, 
+                const double input_11, const double input_12, 
+                const double input_13, double* output_1, 
+                double* output_2, double* output_3);
+void terminate();

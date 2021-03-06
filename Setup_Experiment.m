4b80492dd7b220d499bd644989f6feff7fc8857f
Carleton-SRCL <80171095+Carleton-SRCL@users.noreply.github.com>
2021-03-06 13:24:23 -0500

    Add files via upload

---
+++ Setup_Experiment.m
@@ -0,0 +1,24 @@
+% The following script will create a new SPOT experiment.
+
+function [] = Setup_Experiment()
+
+clear;
+clc;
+close all;
+
+expName = inputdlg('Please enter the name of the experiment (no spaces):');
+
+if isempty(expName)==1
+    fprintf("No name entered...\n");
+else
+    copyfile([cd '\Template_Files\'],...
+    [cd '\Projects\' expName{1,1}]);
+
+    cd([cd '\Projects\' expName{1,1}]);
+
+    movefile('Template_v3_06_2020a.slx',[expName{1,1} '.slx']);
+end
+
+
+
+end
